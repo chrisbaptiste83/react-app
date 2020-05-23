ab4f0236c84a943a0dc9b921464389932c721c8f
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-23 01:22:19 -0700

    update spacing and install tailwind css

--- client/src/components/TacticalItemsList.js
+++ client/src/components/TacticalItemsList.js
@@ -3,8 +3,10 @@ import TacticalItem from './TacticalItem';
 
 const TacticalItemsList = ({tacticalItems}) => {
   return (
-    <React.Fragment>
-      {tacticalItems.map(tItemAttributes => <TacticalItem {...tItemAttributes} />)}
+    <React.Fragment> 
+    
+      {tacticalItems.map(tItemAttributes => <TacticalItem {...tItemAttributes} />)} 
+      
     </React.Fragment>
   )
 }
