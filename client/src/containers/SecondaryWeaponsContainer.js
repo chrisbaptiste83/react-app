fd0b7b49495f66363a79c477031d4990094265b9
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-23 01:13:57 -0700

    add bootstrap styling to secondary weapons container and adjust spacing

--- client/src/containers/SecondaryWeaponsContainer.js
+++ client/src/containers/SecondaryWeaponsContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';  
 import SecondaryWeaponsList from '../components/SecondaryWeaponsList'; 
 import { connect } from 'react-redux';
-import { fetchSecondaryWeapons} from '../actions';
+import { fetchSecondaryWeapons} from '../actions'; 
+import {Jumbotron} from 'react-bootstrap'
 
 
 class SecondaryWeaponsContainer extends Component { 
@@ -12,13 +13,17 @@ class SecondaryWeaponsContainer extends Component {
 
     componentDidMount() {
         this.props.fetchSecondaryWeapons()
-      }
+    }
     
     render() {
         return (
-          <section>
-            <h1>Secondary Weapons:</h1> 
-            <SecondaryWeaponsList secondaryWeapons={this.props.secondaryWeapons}/>
+          <section> 
+            <center>
+            <Jumbotron style={{ width: '50rem' }}>  
+              <h1> Secondary Weapons: </h1> 
+            </Jumbotron>
+            <SecondaryWeaponsList secondaryWeapons={this.props.secondaryWeapons}/> 
+            </center>
           </section>
         )
     }
