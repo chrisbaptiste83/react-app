432cef1d53d6c38a86c1cee9d4d2a04bf3dafb2f
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-23 01:05:15 -0700

    modify actions to include adding a tactical item and updating RECEIVE_TACTICALPACK

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,21 +1,21 @@
 export const FETCHING_TACTICALPACKS = "FETCHING_TACTICALPACKS"
 export const RECEIVE_TACTICALPACKS = "RECEIVE_TACTICALPACKS"
 export const FETCHING_PRIMARYWEAPONS = "FETCHING_PRIMARYWEAPONS"
-export const RECEIVE_PRIMARYWEAPONS = "RECEIVE_PRIMARYWEAPONS"
-export const RECEIVE_TACTICALPACK = "RECEIVE_TACTICALPACK" 
+export const RECEIVE_PRIMARYWEAPONS = "RECEIVE_PRIMARYWEAPONS" 
 export const FETCHING_SECONDARYWEAPONS = "FETCHING_SECONDARYWEAPONS"
 export const RECEIVE_SECONDARYWEAPONS = "RECEIVE_SECONDARYWEAPONS" 
 export const FETCHING_LETHALWEAPONS = "FETCHING_LETHALWEAPONS" 
 export const RECEIVE_LETHALWEAPONS = "RECEIVE_LETHALWEAPONS" 
 export const FETCHING_TACTICALITEMS = "FETCHING_TACTICALITEMS" 
-export const RECEIVE_TACTICALITEMS = "RECEIVE_TACTICALITEMS"
+export const RECEIVE_TACTICALITEMS = "RECEIVE_TACTICALITEMS" 
+export const ADD_TACTICALPACK = "ADD_TACTICALPACK"
 
 export const fetchTacticalPackages = () => {
   return dispatch => {
     dispatch({ type: FETCHING_TACTICALPACKS })
     fetch('http://localhost:3001/tactical_packages')
       .then(res => res.json()) 
-      .then(tacticalPackages => {
+      .then(tacticalPackages => { 
         dispatch({
           type: RECEIVE_TACTICALPACKS, 
           payload: tacticalPackages
@@ -64,6 +64,26 @@ export const fetchSecondaryWeapons = () => {
           })
         })   
     }
+  } 
+
+  export const addTacticalPackage = tacticalPackage => {
+    return dispatch => {
+      return fetch('http://localhost:3001/tactical_packages', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(tacticalPackage)
+        }) 
+          .then(res => res.json())
+          .then(tacticalPackage => { 
+            debugger
+            dispatch({
+              type: ADD_TACTICALPACK,
+              payload: tacticalPackage
+            })
+          })
+    }
   }
 
 export const fetchTacticalPackagebyId = (id) => {
@@ -73,7 +93,7 @@ export const fetchTacticalPackagebyId = (id) => {
         .then(res => res.json()) 
         .then(tacticalPackage => {
           dispatch({
-            type: RECEIVE_TACTICALPACK, 
+            type: RECEIVE_TACTICALPACKS, 
             payload: [tacticalPackage]
           })
         })  
