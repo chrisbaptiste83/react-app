a421654d6f8e6b792a28b2109aaf397a967252b7
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-23 01:15:21 -0700

    add bootstrap styling to primary weapons container

--- client/src/containers/PrimaryWeaponsContainer.js
+++ client/src/containers/PrimaryWeaponsContainer.js
@@ -1,42 +1,49 @@
 import React, { Component } from 'react';  
 import PrimaryWeaponsList from '../components/PrimaryWeaponsList'; 
 import { connect } from 'react-redux';
-import { fetchPrimaryWeapons} from '../actions';
+import { fetchPrimaryWeapons} from '../actions'; 
+import {Jumbotron} from 'react-bootstrap'
 
 class PrimaryWeaponsContainer extends Component { 
 
-constructor(props) {
-  super(props);
-  this.renderLoadedContent = this.renderLoadedContent.bind(this)
+  constructor(props) {
+    super(props);
+    this.renderLoadedContent = this.renderLoadedContent.bind(this)
+  } 
+
+  componentDidMount() {
+    this.props.fetchPrimaryWeapons()
+  } 
+
+  renderLoadedContent() {
+    return (
+      <React.Fragment>
+        <PrimaryWeaponsList primaryWeapons={this.props.primaryWeapons}/>     
+      </React.Fragment>
+    )
+  } 
+
+  render() {
+    return (
+      <section> 
+        <center>
+        <Jumbotron style={{ width: '50rem' }}>  
+            <h1> Primary Weapons: </h1> 
+        </Jumbotron>
+        {this.props.loading ? 'Loading...' : this.renderLoadedContent()} 
+        </center>
+      </section>
+    )
+  }
 } 
 
-componentDidMount() {
-this.props.fetchPrimaryWeapons()
-} 
-
-renderLoadedContent() {
-  return (
-    <React.Fragment>
-      <PrimaryWeaponsList 
-        primaryWeapons={this.props.primaryWeapons}/> 
-    </React.Fragment>
-  )
-} 
-
-render() {
-  return (
-    <section>
-      <h4>Primary Weapons</h4>
-      {this.props.loading ? 'Loading...' : this.renderLoadedContent()}
-    </section>
-  )
+const mapStateToProps = ({primaryWeapons}) => { 
+  
+  return { 
+    primaryWeapons: primaryWeapons.items.map(primaryWeaponId => primaryWeapons.itemsById[primaryWeaponId]),
+    loading: primaryWeapons.loading
+  } 
+  
 }
-} 
 
-const mapStateToProps = ({primaryWeapons}) => {
-return {
-primaryWeapons: primaryWeapons.items.map(primaryWeaponId => primaryWeapons.itemsById[primaryWeaponId]),
-loading: primaryWeapons.loading
-}
-}
 export default connect(mapStateToProps, { fetchPrimaryWeapons })(PrimaryWeaponsContainer)
