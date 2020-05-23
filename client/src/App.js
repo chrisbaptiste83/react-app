dfcf2047dccabea69a836cf69e5ff5be54538ea7
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-23 00:53:37 -0700

    modify file with routes and proper nav nar placement outside of switch statement

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'; 
-import './App.css'; 
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import TacticalPackagesContainer from './containers/TacticalPackagesContainer'; 
 import PrimaryWeaponsContainer from './containers/PrimaryWeaponsContainer'; 
 import SecondaryWeaponsContainer from './containers/SecondaryWeaponsContainer'; 
 import LethalWeaponsContainer from './containers/LethalWeaponsContainer'; 
 import TacticalItemsContainer from './containers/TacticalItemsContainer';  
-import TacticalPackageShow from './containers/TacticalPackageShow';  
-import Navbar from './components/Navbar';
+import TacticalPackageShow from './containers/TacticalPackageShow'; 
+import AddTacticalPackage from './components/AddTacticalPackage'; 
+import Landing from './components/Landing'; 
+import Navigation from './components/Navigation'; 
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,29 +19,29 @@ import {
 function App() {
   return ( 
   <Router> 
-  <div className="w-5/6 mx-auto">  
-  <Switch>
-    <center> 
-    <Navbar/> 
-    <Route exact path="/tactical_packages">
-      <TacticalPackagesContainer /> 
-    </Route> 
-    <Route path="/primary_weapons">
-      <PrimaryWeaponsContainer /> 
-    </Route> 
-    <Route path="/tactical_items">
-      <TacticalItemsContainer /> 
-    </Route> 
-    <Route path="/secondary_weapons">
-      <SecondaryWeaponsContainer />  
-    </Route>
-    <Route path="/lethal_weapons">
-      <LethalWeaponsContainer /> 
-    </Route> 
-    <Route path="/tactical_packages/:id" component={TacticalPackageShow}></Route>
-    </center> 
-  </Switch> 
-  </div> 
+      <Navigation/>
+        <Switch> 
+          <Route exact path="/">
+            <Landing /> 
+          </Route> 
+          <Route exact path="/tactical_packages">  
+            <TacticalPackagesContainer /> 
+          </Route> 
+          <Route path="/primary_weapons">
+            <PrimaryWeaponsContainer /> 
+          </Route> 
+          <Route path="/tactical_items">
+            <TacticalItemsContainer /> 
+          </Route> 
+          <Route path="/secondary_weapons">
+            <SecondaryWeaponsContainer />  
+          </Route>
+          <Route path="/lethal_weapons">
+            <LethalWeaponsContainer />
+          </Route> 
+          <Route path="/tactical_packages/:id" component={TacticalPackageShow}/>  
+          <Route path="/new_tactical_package" component={AddTacticalPackage}/>
+        </Switch> 
   </Router>
   );
 }
