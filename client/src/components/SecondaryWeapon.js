1d86153572e2f7e19de30ee8cbab6b7e1465e21d
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-23 01:21:18 -0700

    modify with bootstrap

--- client/src/components/SecondaryWeapon.js
+++ client/src/components/SecondaryWeapon.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {Card} from 'react-bootstrap'
 
 const SecondaryWeapon = ({name, description, image_url}) => {
   return (
     <React.Fragment>
-      <h1>{name}</h1> - {description}<p><img 
-      src={image_url} 
-        width={500}
-        height={240}
-        alt =""
-      /></p>
+      <Card style={{ width: '50rem' }}>  
+        <Card.Body>
+          <Card.Title><h2>{name}</h2></Card.Title> 
+          <Card.Text><h5>{description}</h5></Card.Text> 
+            <p><img src={image_url} width={500} height={240} alt=""/></p> 
+        </Card.Body>
+      </Card><br></br>
     </React.Fragment>
   )
 }
