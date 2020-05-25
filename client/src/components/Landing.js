a30d8fdbed449f5a199f537498bf17756733c812
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-25 03:13:04 -0700

    modify Landing page styling and link to tactical packs

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,16 @@
 import React from 'react'; 
-import {Button, Jumbotron} from 'react-bootstrap';
+import {Button, Jumbotron} from 'react-bootstrap'; 
+import { Link } from 'react-router-dom'
 
 
 const Landing = () => {
     return (  
          <section> 
             <center>
-              <Jumbotron style={{ width: '50rem'}}>
-                <h1>Zombie Apocalypse Tactical Packs</h1>
-                <p>Share your favorite tactical packages and prepare for doomsday like no other.</p>
-                <p><Button variant="primary">Browse Tactical Packages</Button></p>
+              <Jumbotron style={{ background: 'rgba(0,0,0, 0.65)', width: '80rem', color:'white'}}>
+                <h1>Zombie Apocalypse Tactical Packs</h1><br></br>
+                <h5>Share your favorite tactical packages and prepare for doomsday like no other.</h5><br></br>
+                <p><Button variant="dark"><Link to={{pathname: `/tactical_packages`}}>See Tactical Packs</Link></Button></p>
               </Jumbotron>
             </center>
           </section>   
