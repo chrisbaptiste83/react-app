9c2089fbd82539644e683253f1cf232cf5cb7a1c
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-05-25 03:15:59 -0700

    update navigation bar with links and update styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap'
+import { Navbar, Nav} from 'react-bootstrap'; 
+import { Link } from 'react-router-dom'; 
 
 const Navigation = () => {
   return (
-    <section id="navigation"> 
-      <Navbar bg="dark" variant="dark" fixed="top" style={{ height: '5rem' }}>
-      <Navbar.Brand href="/tactical_packages"><h4>Zombie Apocalypse Tactical Packs</h4></Navbar.Brand>
-      <Nav className="mr-auto">
-          <Nav.Link  href="/primary_weapons"><h4>Primary Weapons</h4></Nav.Link>
-          <Nav.Link href="/secondary_weapons"><h4>Secondary Weapons</h4></Nav.Link>
-          <Nav.Link href="/lethal_weapons"><h4>Lethal Weapons</h4></Nav.Link>
-          <Nav.Link href="/tactical_items"><h4>Tactical Items</h4></Nav.Link>
-          <Nav.Link href="/new_tactical_package"><h4>Create</h4></Nav.Link>      
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
+    <section>
+      <Navbar  variant="dark" fixed="top" style={{ height: '6rem', background:'rgba(0,0,0, 0.60)'}}>
+      <Nav.Link><Link to={{ pathname: `/tactical_packages/`}} style={{ color: '#FFF' }}><h2>Zombie Tactical Packs</h2></Link></Nav.Link>
+      <Nav.Link><Link to={{ pathname: `/primary_weapons/`}} style={{ color: '#FFF' }}><h2>Primary Weapons</h2></Link></Nav.Link>   
+      <Nav.Link><Link to={{ pathname: `/secondary_weapons/`}} style={{ color: '#FFF' }}><h2>Secondary Weapons</h2></Link></Nav.Link>     
+      <Nav.Link><Link to={{ pathname: `/lethal_weapons/`}} style={{ color: '#FFF' }}><h2>Lethal Weapons</h2></Link></Nav.Link>     
+      <Nav.Link><Link to={{ pathname: `/tactical_items/`}} style={{ color: '#FFF' }}><h2>Tactical Items</h2></Link></Nav.Link>     
+      <Nav.Link><Link to={{ pathname: `/new_tactical_package/`}} style={{ color: '#FFF' }}><h2>Create</h2></Link></Nav.Link>     
       </Navbar>
     </section>
   )
