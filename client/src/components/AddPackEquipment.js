edcb198996f0aca1a30036d0a69731ac272e3750
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-06-01 15:18:54 -0700

    create AddPackEquipment component for adding equipment to tactical packs. Include components of all forms to add equipment

---
+++ client/src/components/AddPackEquipment.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import AddPrimaryWeapon from './AddPrimaryWeapon'; 
+import AddSecondaryWeapon from './AddSecondaryWeapon'; 
+import AddLethalWeapon from './AddLethalWeapon'; 
+import AddTacticalItem from './AddTacticalItem';
+import PackageListItem from './PackageListItem';
+import {fetchTacticalPackagebyId} from '../actions/index.js'; 
+import { connect } from 'react-redux'; 
+
+
+
+class AddPackEquipment extends Component { 
+
+    
+      componentDidMount() { 
+        this.props.fetchTacticalPackagebyId(this.props.match.params.id)
+      } 
+    
+    render() {
+      return (  
+        <center> 
+        <PackageListItem {...this.props.tacticalPackage}/>
+          <React.Fragment>
+                <AddPrimaryWeapon {...this.props.tacticalPackage}/><br></br> 
+                <AddSecondaryWeapon {...this.props.tacticalPackage}/><br></br> 
+                <AddLethalWeapon {...this.props.tacticalPackage}/><br></br> 
+                <AddTacticalItem {...this.props.tacticalPackage}/><br></br>               
+        </React.Fragment> 
+        </center>
+      ) 
+    }
+  }
+  
+  const mapStateToProps = ({tacticalPackages},{match}) => {
+    return {
+     tacticalPackage: tacticalPackages.itemsById[match.params.id]
+    }
+  }
+  export default connect(mapStateToProps, { fetchTacticalPackagebyId })(AddPackEquipment)
+
+
+              
