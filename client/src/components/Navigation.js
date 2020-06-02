94d2d4d1e16a8987c07f0ce43ab96f30b4d72d81
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-06-02 03:01:23 -0700

    add home page link to Laning component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <div>
-      <Navbar  variant="dark" fixed="top" style={{ height: '6rem', background:'rgba(0,0,0, 0.60)'}}>
+      <Navbar  variant="dark" fixed="top" style={{ height: '6rem', background:'rgba(0,0,0, 0.60)'}}> 
+      <Nav.Link><Link to={{ pathname: `/`}} style={{ color: '#FFF' }}><h2>Home</h2></Link></Nav.Link>
       <Nav.Link><Link to={{ pathname: `/tactical_packages/`}} style={{ color: '#FFF' }}><h2>Zombie Tactical Packs</h2></Link></Nav.Link>
       <Nav.Link><Link to={{ pathname: `/primary_weapons/`}} style={{ color: '#FFF' }}><h2>Primary Weapons</h2></Link></Nav.Link>   
       <Nav.Link><Link to={{ pathname: `/secondary_weapons/`}} style={{ color: '#FFF' }}><h2>Secondary Weapons</h2></Link></Nav.Link>     
