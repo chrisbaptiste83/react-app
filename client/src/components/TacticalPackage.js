d8d265dc2b3f3604abb71de73db8f0c7a64ec924
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-06-02 03:13:09 -0700

    update spacing and component styling

--- client/src/components/TacticalPackage.js
+++ client/src/components/TacticalPackage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card} from 'react-bootstrap';
 
 
-const TacticalPackage = ({tacticalPackage:{id, title, description, creator, secondary_weapon, primary_weapon, lethal_weapon, tactical_item}}) => { 
+const TacticalPackage = ({tacticalPackage:{title, description, creator, secondary_weapon, primary_weapon, lethal_weapon, tactical_item}}) => { 
  
   return (
     <React.Fragment>
@@ -18,37 +18,37 @@ const TacticalPackage = ({tacticalPackage:{id, title, description, creator, seco
         </Card><br></br> 
         <Card style={{ background: 'rgba(0,0,0, 0.85)',color:'white', width: '65rem' }}>  
           <Card.Body>
-            <Card.Title><h2>Primary Weapon:</h2><p><h2>{primary_weapon.name}</h2></p></Card.Title> 
-            <p><img src={primary_weapon.image_url} width={500} height={240} alt=""/></p> 
+            <Card.Title><h2>Primary Weapon:</h2><br></br><h2>{primary_weapon.name}</h2></Card.Title> 
+            <br></br><img src={primary_weapon.image_url} width={500} height={240} alt=""/>
             <Card.Text>
-              <h5>{primary_weapon.description}</h5><br></br> 
+            <br></br><h5>{primary_weapon.description}</h5><br></br> 
             </Card.Text> 
           </Card.Body>
         </Card><br></br> 
         <Card style={{ background: 'rgba(0,0,0, 0.85)',color:'white', width: '65rem' }}>  
           <Card.Body>
-            <Card.Title><h2>Secondary Weapon:</h2><p><h2>{secondary_weapon.name}</h2></p></Card.Title> 
-            <p><img src={secondary_weapon.image_url} width={500} height={240} alt=""/></p> 
+            <Card.Title><h2>Secondary Weapon:</h2><br></br><h2>{secondary_weapon.name}</h2></Card.Title> 
+            <br></br><img src={secondary_weapon.image_url} width={500} height={240} alt=""/> 
             <Card.Text>
-              <h5>{secondary_weapon.description}</h5><br></br> 
+            <br></br><h5>{secondary_weapon.description}</h5><br></br> 
             </Card.Text> 
           </Card.Body>
         </Card><br></br> 
         <Card style={{background: 'rgba(0,0,0, 0.85)',color:'white', width: '65rem' }}>  
           <Card.Body>
-            <Card.Title><h2>Lethal Weapon:</h2><p><h2>{lethal_weapon.name}</h2></p></Card.Title> 
-            <p><img src={lethal_weapon.image_url} width={500} height={240} alt=""/></p> 
+            <Card.Title><h2>Lethal Weapon:</h2><br></br><h2>{lethal_weapon.name}</h2></Card.Title> 
+            <br></br><img src={lethal_weapon.image_url} width={500} height={240} alt=""/> 
             <Card.Text>
-              <h5>{lethal_weapon.description}</h5><br></br> 
+              <br></br><h5>{lethal_weapon.description}</h5><br></br> 
             </Card.Text> 
           </Card.Body>
         </Card><br></br>
         <Card style={{background: 'rgba(0,0,0, 0.85)',color:'white', width: '65rem' }}>  
           <Card.Body>
-            <Card.Title><h2>Tactical Equipment:</h2><p><h2>{tactical_item.name}</h2></p></Card.Title> 
-            <p><img src={tactical_item.image_url} width={500} height={240} alt=""/></p> 
+            <Card.Title><h2>Tactical Equipment:</h2><br></br><h2>{tactical_item.name}</h2></Card.Title> 
+            <br></br><img src={tactical_item.image_url} width={500} height={240} alt=""/>
             <Card.Text>
-              <h5>{tactical_item.description}</h5><br></br> 
+              <br></br><h5>{tactical_item.description}</h5><br></br> 
             </Card.Text> 
           </Card.Body>
         </Card><br></br>    
