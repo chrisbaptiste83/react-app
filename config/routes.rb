e0aca7a68ae4f4c58acd3fdae22ba1e441c90c11
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-06-16 18:30:16 -0700

    create routes for logging in and out with sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   resources :lethal_weapons
   resources :secondary_weapons
   resources :primary_weapons 
+
+  get "/login/status", to: "sessions#is_logged_in?"
+  delete "/logout/:id", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
   
 end
