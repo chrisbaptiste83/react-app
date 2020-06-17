25de090382b59c4fb30379135b3ed83dc4cb037a
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-06-16 17:51:48 -0700

    create session controller and modify user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+
+    wrap_parameters :user, include: [:username, :email, :password, :password_confirmation]
+
+    def create
+        @user = User.create!(user_params)
+        if @user
+            session[:user_id] = @user.id
+            render json: {
+                status: :created,
+                user: UserSerializer.new(@user)
+            }
+        else 
+            render json: {
+                status: 500,
+                errors: @user.errors.full_messages
+            }
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
 end
